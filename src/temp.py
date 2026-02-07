8d48394760f8979fc66ae9bb97b7a1cf96dce032
Albert Sjåvåg <albertsj@ulrik.uio.no>
2026-02-07 14:36:49 +0100

    Just displaying different stock data.

--- src/temp.py
+++ src/temp.py
@@ -2,12 +2,12 @@
 Just a check to see if data looks ok. Example use temporarily
 """
 
-from src.data import fetch_adj_close, clean_prices
+from src.data import fetch_prices, clean_prices
 
-tickers = ["EQNR.OL", "DNB.OL", "TEL.OL", "AAPL", "MSFT"]
-prices, fetch_report = fetch_adj_close(tickers, years=3)
+tickers = ["EQNR.OL", "AAPL", "MSFT", "ETH-USD"]
+prices, fetch_report = fetch_prices(tickers, years=3)
 prices, clean_report = clean_prices(prices, method="dropna", min_obs=252)
 
 print(fetch_report)
 print(clean_report)
-print(prices.head(20))
+print(prices)  # .head(180)
